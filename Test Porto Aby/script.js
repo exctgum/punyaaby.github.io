36113e0c32f7f4529b03666b712317841e8b4a04
exctgum <141287168+exctgum@users.noreply.github.com>
2023-08-02 18:53:53 -0700

    Delete Test Porto Aby directory

--- Test Porto Aby/script.js
+++
@@ -1,8 +0,0 @@
-// Animasi saat load halaman
-window.addEventListener('load', function () {
-    const elementsToAnimate = document.querySelectorAll('.project-item');
-
-    elementsToAnimate.forEach(function (element, index) {
-        element.style.animation = `fadeIn 0.5s ease ${index * 0.2}s`;
-    });
-});
