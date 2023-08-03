275b7ff379067d5cf443a27b5546f3006618bac9
exctgum <141287168+exctgum@users.noreply.github.com>
2023-08-03 20:51:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,32 @@ window.addEventListener('load', function () {
 
     elementsToAnimate.forEach(function (element, index) {
         element.style.animation = `fadeIn 0.5s ease ${index * 0.2}s`;
+
+        // script.js
+
+// Ambil elemen tombol toggle mode
+const toggleBtn = document.querySelector('.toggle-btn');
+
+// Cek apakah mode sebelumnya adalah light mode atau dark mode
+const currentMode = localStorage.getItem('mode');
+if (currentMode) {
+    document.body.classList.add(currentMode);
+}
+
+// Fungsi untuk mengganti mode
+function toggleMode() {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('mode', 'light-mode');
+    } else {
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('mode', 'dark-mode');
+    }
+    document.body.classList.add(localStorage.getItem('mode'));
+}
+
+// Tambahkan event listener untuk tombol toggle
+toggleBtn.addEventListener('click', toggleMode);
+
     });
 });
